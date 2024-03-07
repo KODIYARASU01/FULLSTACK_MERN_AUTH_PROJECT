9b34b2c5ef9e5c1f6f2ae4252d768c6e2e2d0fd1
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-07 17:33:24 +0000

    Client Side SignUp form created

--- client/src/Component/Pages/SignUp.jsx
+++ client/src/Component/Pages/SignUp.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import React from "react";
+import "./SignUp.scss";
 const SignUp = () => {
   return (
-    <div>
-      <h2>SignUp Page</h2>
-    </div>
-  )
-}
+    <>
+      <div className="signup_container">
+        <div className="signup_title">
+          <h4>Sign Up</h4>
+        </div>
+
+        <div className="signUp_container">
+          <form action="">
+            <div className="form_group">
+              <label htmlFor="userName">UserName</label>
+              <input
+                type="text"
+                name="userName"
+                id="UserName"
+                placeholder="Enter Unique UserName"
+              />
+            </div>
+            <div className="form_group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter Unique Email Address"
+              />
+            </div>
+            <div className="form_group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter Strong password"
+              />
+            </div>
+
+            <div className="form_submit">
+              <button type="submit">SignUp</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
