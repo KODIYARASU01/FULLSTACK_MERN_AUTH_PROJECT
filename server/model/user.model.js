7cbf9a1eaff65d34aa235d68aafd92831aefabbb
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-07 14:27:16 +0000

    Created User.Model.js  for user signup form

---
+++ server/model/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+let userSchema = new mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+let User = mongoose.model("User", userSchema);
+
+export default User;
