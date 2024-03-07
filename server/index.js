cfd9ed13b203a07cf29708b3fa0f2d6b66a3f8a1
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-07 13:57:07 +0000

    Mongodb connection string established

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
-let PORT=process.env.PORT ||5000;
+//Port number initialize:
+let PORT = process.env.PORT || 5000;
+//Mongodb conncetion initializing:
+let uri = process.env.MONGODB_CONECTION_STRING;
 
-let app=express();
-
-app.get('/',(req,res)=>{
-    res.send('Server is working')
-})
-
-app.listen(PORT,()=>{
-    console.log(`Server running ${PORT} port number`)
+//App Initialize:
+let app = express();
+//Home route:
+app.get("/", (req, res) => {
+  res.send("Server is working");
 });
-
+//Connection to database :
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("Mongoose Connected Sucessfully");
+    try {
+      //Application listening port number
+      app.listen(PORT, () => {
+        console.log(`Server running ${PORT} port number`);
+      });
+    } catch (err) {
+      console.log("Server is not conected");
+    }
+  })
+  .catch((error) => {
+    console.log("Mongoose conncetion failed ", error.message);
+  });
