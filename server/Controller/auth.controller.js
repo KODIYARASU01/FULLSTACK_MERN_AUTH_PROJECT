2d256f1317f2f40fd5d1d9517bffd11b1dd3c992
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-07 15:56:57 +0000

    Custom Error handling created on utils folder

--- server/Controller/auth.controller.js
+++ server/Controller/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
-
+import { errorHandler } from "../utils/error.js";
 export const signUp = async (req, res, next) => {
   let { userName, email, password } = req.body;
 
@@ -9,9 +9,15 @@ export const signUp = async (req, res, next) => {
   let newUser = new User({ userName, email, password: hashedPassword });
   try {
     if (!userName || !email || !password) {
-      return res.status(201).json({
-        message: "Make sure to fill required Fields : userName,email,password",
-      });
+      //   return res.status(201).json({
+      //     message: "Make sure to fill required Fields : userName,email,password",
+      //   });
+      next(
+        errorHandler(
+          401,
+          "Make sure to fill all required Fields : userName,email,password"
+        )
+      );
     }
     await newUser.save();
     return res.status(201).json({ message: "User Created Sucessfully" });
